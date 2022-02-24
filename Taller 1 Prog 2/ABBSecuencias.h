d954f10a0f8926a8890864ab23d29250bc2163e1
letidonatti <68922655+letidonatti@users.noreply.github.com>
2022-02-24 10:25:12 -0300

    Update ABBSecuencias.h

--- Taller 1 Prog 2/ABBSecuencias.h
+++ Taller 1 Prog 2/ABBSecuencias.h
@@ -16,22 +16,22 @@ void CrearArbol (ABBSecuencias &a);
 // Crea arbol vacio
 
 boolean esVacioABB (ABBSecuencias a);
-//devuelve TRUE si el �rbol est� vac�o
+//devuelve TRUE si el arbol esta vacio
 
 Secuencia DarRaiz (ABBSecuencias a); ///Pre condicion: ABB no vacio
-//devuelve el estructurado del nodo raiz del �rbol pasado como par�metro
+//devuelve el estructurado del nodo raiz del arbol pasado como parametro
 
-Secuencia devuelveSec (ABBSecuencias a, stringD ident); /// Pre condicion: existeSecuencia ident en el �rbol a
-//dado un identificador devuelve la secuencia correspondiente cargado en el �rbol
+Secuencia devuelveSec (ABBSecuencias a, stringD ident); /// Pre condicion: existeSecuencia ident en el arbol a
+//dado un identificador devuelve la secuencia correspondiente cargado en el arbol
 
 ABBSecuencias HijoIzq (ABBSecuencias a); ///Pre condicion: ABB no vacio
-//devuelve el hijo izq del�rbol pasado como par�metro
+//devuelve el hijo izq del arbol pasado como parametro
 
 ABBSecuencias HijoDer (ABBSecuencias a); ///Pre condicion: ABB no vacio
-//devuelve el hijo der del �rbol pasado como par�metro
+//devuelve el hijo der del arbol pasado como parametro
 
 void insertarSecuencia (ABBSecuencias &a, Secuencia sec); /// Pre condicion: La secuencia no existe en el arbol
-//inserta una secuencia en el �rbol ordenada alfab�ticamente seg�n su identificador
+//inserta una secuencia en el arbol ordenada alfabeticamente segun su identificador
 
 boolean Pertenece (ABBSecuencias a, stringD ident);
 //Devueluve TRUE si una secuencia pertenece al arbol
@@ -40,13 +40,13 @@ void modificaArbol (ABBSecuencias &a, Secuencia sec);
 //modifica la secuencia pasada por parámetro en el ABB
 
 void mostrarEnOrden (ABBSecuencias a); ///Pre condicion: ABB no vacio
-//recorre el �rbol de la forma: izq-ra�z-der y muestra en pantalla las secuencias en ese orden
+//recorre el arbol de la forma: izq-raiz-der y muestra en pantalla las secuencias en ese orden
 
 ABBSecuencias constructABB (Secuencia raiz, ABBSecuencias i, ABBSecuencias  d);
 //dada una secuencia raiz, construye el ABB.
 
 void destruirABB (ABBSecuencias &a);
-// libera la memoria ocupada por el �rbol y lo deja nulo
+// libera la memoria ocupada por el arbol y lo deja nulo
 
 
 
