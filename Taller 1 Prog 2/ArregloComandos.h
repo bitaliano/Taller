35a6cecd4fb6f2f034dda0b6496b6d0049134cee
letidonatti <68922655+letidonatti@users.noreply.github.com>
2022-02-24 10:26:59 -0300

    Update ArregloComandos.h

--- Taller 1 Prog 2/ArregloComandos.h
+++ Taller 1 Prog 2/ArregloComandos.h
@@ -10,10 +10,10 @@ void levantarComandos (ArregloComandos &arreC, stringD fileName);
 //abre el archivo y carga el arreglo con los comandos levantados del archivo
 
 void n_esimoComando (ArregloComandos arre, int num, stringD &s);
-//devuelve el string cargado en la posici�n num del arreglo
+//devuelve el string cargado en la posicion num del arreglo
 
 boolean existeComando (ArregloComandos arre, stringD s);
-//devuelve TRUE si el string pasado por par�metro es un comando existente en el arreglo
+//devuelve TRUE si el string pasado por parametro es un comando existente en el arreglo
 
 void destruirComandos (ArregloComandos arre);
 //recorre el arreglo y destruye los strings
